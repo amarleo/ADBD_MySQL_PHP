6fca73babea1eff94897fee710c35f7b6b0dec86
Alejandro Martín de León <alu0101015941@ull.edu.es>
2022-01-09 17:06:23 +0000

    now it can search clients for more than one column

--- cliente.php
+++ cliente.php
@@ -1,4 +1,5 @@
 <?php
+header('Content-Type: text/html; charset=UTF-8');
 session_start(); 
 require "bbdd.php"; 
 $_SESSION['display_cliente'] = TRUE;
@@ -25,7 +26,52 @@ if ($result = mysqli_query($db, "SELECT * FROM CLIENTE ORDER BY $column $sort_or
 </head>
 
 <body>
+
+  <?php
+    if(isset($_GET['search'])) {
+      $fields = array('DNI', 'Nombre', 'Apellidos', 'Email', 'Telefono', 'Direccion', 'Codigo_Postal');
+      $conditions = array();
+
+      foreach($fields as $field) {
+        //if the field is set and not empty
+        if(isset($_POST[$field]) && $_POST[$field] != '') {
+          $conditions[] = " $field = '" . mysqli_real_escape_string($db,$_POST[$field]) . "'";
+        }
+      }
+      $query = "SELECT * FROM CLIENTE";
+      if(count($conditions) > 0) {
+        $query .= " WHERE " . implode(" AND ", $conditions);
+      }
+      echo $query;
+      $result = mysqli_query($db, $query);
+    }
+  ?>
+
 <table>
+  <h1>Búsqueda</h1>
+  <tr>
+    <th>DNI</th>
+    <th>Nombre</th>
+    <th>Apellidos</th>
+    <th>Email</th>
+    <th>Teléfono</th>
+    <th>Direccion</th>
+    <th>Código Postal</th>
+    </tr>
+
+    <form action="cliente.php?search=DNI" method="post">
+    <td><input type='text' name='DNI'></td>
+    <td><input type='text' name='Nombre'></td>
+    <td><input type='text' name='Apellidos'></td>
+    <td><input type='text' name='Email'></td>
+    <td><input type='text' name='Telefono'></td>
+    <td><input type='text' name='Direccion'></td>
+    <td><input type='text' name='Codigo_Postal'></td>
+    <td><button>Insert</button></td>
+    </form>
+  </table>
+
+  <table>
   <h1>Tabla Cliente</h1>
   <tr>
     <th><a href="cliente.php?column=DNI&order=<?php echo $asc_or_desc; ?>">DNI<i class ="<?php echo $column == 'DNI' ? '-' . $up_or_down : ''; ?>"></i></a></th>
@@ -53,6 +99,8 @@ if ($result = mysqli_query($db, "SELECT * FROM CLIENTE ORDER BY $column $sort_or
     </form>
   </tr>
 <?php
+
+
   while($data = mysqli_fetch_assoc($result)) {
     if ($data['Borrado'] == 0) { 
   ?>
